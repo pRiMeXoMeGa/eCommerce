e0deeceda4216486e079b1f0f0cefe6d2b4ed7b3
pRiMeXoMeGa <pRiMeXoMeGa@github.com>
2022-10-30 14:01:30 +0530

    added login, cart, registration components

--- frontEnd/frontEndPart/src/app/app.module.ts
+++ frontEnd/frontEndPart/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent,
+    RegistrationComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
